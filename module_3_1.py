ed06635bd7ad72cf8fbf45e6be85304b5df8a785
kcissel <golovina.nastya02@gmail.com>
2024-07-02 21:13:14 +0500

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -7,7 +7,7 @@ def string_info(string):
     return (len(string), string.upper(), string.lower())
 def is_contains (string, list_to_search):
     count_calls()
-    return string.upper() in [s.upper() for s in list_to_search]
+    return string.lower() in [s.lower() for s in list_to_search]
 
 
 print(string_info('Paramedic'))
